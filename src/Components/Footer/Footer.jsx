57d6e9af6871cbe7363fcfb38eb1cd40c828e9d9
Bulbash93 <koriba93@gmail.com>
2025-08-27 09:15:28 +0000

    main

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,26 +1,35 @@
-import "./Footer.css"
+import "./Footer.css";
 
 export default function Footer() {
-     const Mailto = ({email, children}) => {
-        return <a href={`mailto:${email}`}>{children}</a>
-    }
+  const Mailto = ({ email, children }) => {
+    return <a href={`mailto:${email}`}>{children}</a>;
+  };
 
-    const Callto = ({ phone, children }) => {
-        return <a href={`tel:${phone}`}>{children}</a>;
-    }
+  const Callto = ({ phone, children }) => {
+    return <a href={`tel:${phone}`}>{children}</a>;
+  };
 
-    return (
-        <footer class="footer">
-        <div class="container container__footer">
-            <div class="footer-info">
-            <a href="#" class="logo">dk</a>
-                <div class="contact__info">
-                   <Mailto  email={"koriba93@gmail.com"}><strong>Email: </strong>koriba93@gmail.com</Mailto>
-                    <Callto phone="+79165287796"><strong>tel: </strong>+7 916 529 77 96</Callto>
-                    <a href="https://github.com/Bulbash93" target="_blank"> <strong>Git: </strong>Личный кабинет</a>
-                </div>
-            </div>        
+  return (
+    <footer class="footer">
+      <div class="container container__footer">
+        <div class="footer-info">
+          <a href="#" class="logo">
+            dk
+          </a>
+          <div class="contact__info">
+            <Mailto email={"koriba93@gmail.com"}>
+              <strong>Email: </strong>koriba93@gmail.com
+            </Mailto>
+            <Callto phone="+79165287796">
+              <strong>Tel: </strong>+7 916 528 77 96
+            </Callto>
+            <a href="https://github.com/Bulbash93" target="_blank">
+              {" "}
+              <strong>Git: </strong>Личный кабинет
+            </a>
+          </div>
         </div>
+      </div>
     </footer>
-    )
+  );
 }
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,15 +1,29 @@
-import "./Projects.css"
+import "./Projects.css";
 
 export default function Projects() {
-    return (
-        <section id="projects" class="section section__projects">
-        <div class="container container__projects">
-            <h2 class="projects__title">Проекты</h2>
-            <div class="project">
-                <p class="project__title">Логистическая компания</p>
-                <a class="project__link" href="#"><button class="project__btn">Сылка на проект</button></a>
-            </div>
+  return (
+    <section id="projects" class="section section__projects">
+      <div class="container container__projects">
+        <h2 class="projects__title">Проекты</h2>
+        <div class="project">
+          <p class="project__title">Логистическая компания</p>
+          <a
+            href="https://github.com/Bulbash93/Logistic"
+            className="project__link-btn button"
+          >
+            Сылка на проект
+          </a>
         </div>
+        <div class="project project__aniclot">
+          <p class="project__title">Aniclot</p>
+          <a
+            href="https://github.com/Bulbash93/Aniclot"
+            className="project__link-btn button"
+          >
+            Сылка на проект
+          </a>
+        </div>
+      </div>
     </section>
-    )
+  );
 }
